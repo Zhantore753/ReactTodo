c08d3523047aa695acf3a7acd1a1655b8154b3db
Zhantore <753gg@mail.ru>
2020-11-21 19:26:37 +0600

    React_Todo

---
+++ src/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Modal.css';
+
+export default class Modal extends React.Component{
+    state = {
+        isOpen: false
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <button onClick={()=>{this.setState({isOpen: true})}}>Open Modal</button>
+
+                {this.state.isOpen && (<div className="modal">
+                    <div className="modal-body">
+                        <h1>Todo list</h1>
+                        <p>Just todo list</p>
+                        <button onClick={()=>{this.setState({isOpen: false})}}>Close modal</button>
+                    </div>
+                </div>)}
+            </React.Fragment>
+        )
+    }
+}
